ecb168b7b669bbd3356be290cf25fdf0a6977819
Gaelpem <pembelegael@gmail.com>
2025-03-09 14:04:46 +0100

    style

--- projet-biographie.php
+++ projet-biographie.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="../asset/css/style.css"/>
+<link rel="stylesheet" href="asset/css/style.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -9,7 +9,7 @@
     <title>Biographie</title>
 </head>
 <body>
-<?php include "../inc/navbar.inc.php" ?>
+<?php include "inc/navbar.inc.php" ?>
 
 
 <main class="portfolio-photo">
@@ -18,8 +18,8 @@
 </main>
 
 
-<?php include "../inc/footer.inc.php"?>
-<script src="../asset/js/app.js"></script>
+<?php include "inc/footer.inc.php"?>
+<script src="asset/js/app.js"></script>
 
 </body>
 </html>
